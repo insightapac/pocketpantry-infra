87e63764dd05f806068a18811872733810dd27c2
James Bannan <james@bannan.com.au>
2019-04-07 09:57:22 +1000

    Added resource location

--- infrastructure/modules/container-registry/main.tf
+++ infrastructure/modules/container-registry/main.tf
@@ -1,5 +1,6 @@
 data "azurerm_container_registry" "acr" {
   name                  = "${var.acr_name}"
+  location              = "${var.acr_location}"
   resource_group_name   = "${var.resource-group_name}"
   sku                   = "${var.acr_sku}"
   tags                  = "${var.resource-tags}"
--- infrastructure/modules/container-registry/vars.tf
+++ infrastructure/modules/container-registry/vars.tf
@@ -6,6 +6,10 @@ variable "acr_name" {
     description = "(Required) The name of the Azure Container Service resource."
     default     = ""
 }
+variable "acr_location" {
+    description = "(Required) The Azure location where the Azure Container Service will be deployed."
+    default     = ""
+}
 variable "sku" {
     description = "(Required) The SKU of the API Management resource."
     default     = "Basic"
