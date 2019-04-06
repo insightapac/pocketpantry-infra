2b988b7cebe75d1dcc8ea1704dfc46dc90313e87
James Bannan <james@bannan.com.au>
2019-04-06 16:27:06 +1100

    Added account name and resource group details

--- infrastructure/modules/cosmos-db-collection/main.tf
+++ infrastructure/modules/cosmos-db-collection/main.tf
@@ -1,10 +1,12 @@
 resource "null_resource" "cosmosdb-collection" {
   provisioner "local-exec" {
-    command = "az cosmosdb collection create --db-name $(echo $cosmosdb_database_name) --collection-name $(echo $cosmosdb_collection_name) --verbose"
+    command = "az cosmosdb collection create --db-name $(echo $cosmosdb_database_name) --collection-name $(echo $cosmosdb_collection_name) --name $(echo $cosmosdb_account_name) --resource-group-name $(echo $resource_group_name)--verbose"
     interpreter = ["/bin/bash", "-c"]
     environment {
-        cosmosdb_database_name    = "${var.cosmosdb_database_name}"
-        cosmosdb_collection_name  = "${var.cosmosdb_collection_name}"
+      cosmosdb_account_name     = "${var.cosmosdb_account_name}"
+      cosmosdb_database_name    = "${var.cosmosdb_database_name}"
+      cosmosdb_collection_name  = "${var.cosmosdb_collection_name}"
+      resource_group_name       = "${var.resource_group_name}"
     }
   }
 }
