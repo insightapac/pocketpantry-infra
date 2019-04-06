1982049f70e98f8badde45b176d5d5169adb6c08
James Bannan <james@bannan.com.au>
2019-04-06 22:20:59 +1100

    Re-added variable references

--- infrastructure/modules/cosmos-db-collection/vars.tf
+++ infrastructure/modules/cosmos-db-collection/vars.tf
@@ -6,3 +6,11 @@ variable "cosmosdb_collection_name" {
   description = "(Required) The name of the CosmosDB collection."
   default     = ""
 }
+variable "cosmosdb_account_name" {
+  description = "(Required) The name of the CosmosDB account."
+  default     = ""
+}
+variable "resource_group_name" {
+    description = "(Required) The name of the Resource Group."
+    default     = ""
+}
