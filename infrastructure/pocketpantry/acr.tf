fcdbc8e34354f5015b2454e12143238c5c3b7cc6
James Bannan <james@bannan.com.au>
2019-04-07 09:59:54 +1000

    Corrected module name reference

--- infrastructure/pocketpantry/acr.tf
+++ infrastructure/pocketpantry/acr.tf
@@ -1,5 +1,5 @@
 module "acr" {
-  source                = "../modules/acr"
+  source                = "../modules/container-registry"
   resource-group_name   = "${module.resource-group.resource-group_name}"
   acr_name              = "${local.solution-name-lower}acr"
   acr_location          = "${module.resource-group.resource-group_location}"
