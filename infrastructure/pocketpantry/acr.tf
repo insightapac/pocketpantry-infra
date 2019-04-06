0df38c78dd1c27006e589f4cfef5c3e9949dcc2a
James Bannan <james@bannan.com.au>
2019-04-07 09:57:33 +1000

    Manifest to deploy ACR resource

---
+++ infrastructure/pocketpantry/acr.tf
@@ -0,0 +1,7 @@
+module "acr" {
+  source                = "../modules/acr"
+  resource-group_name   = "${module.resource-group.resource-group_name}"
+  acr_name              = "${local.solution-name-lower}acr"
+  acr_location          = "${module.resource-group.resource-group_location}"
+  resource-tags         = "${module.resource-group.resource-group_tags}"
+}
