63753eea408b7b4d869af6377b8b2cb762caf413
James Bannan <james@bannan.com.au>
2019-04-06 18:15:58 +1100

    Removed database and collection for testing

--- infrastructure/pocketpantry/cosmosdb.tf
+++ infrastructure/pocketpantry/cosmosdb.tf
@@ -9,14 +9,3 @@ module "cosmosdb" {
   cosmosdb_account_maxstaleness         = 100
   cosmosdb_account_failover_location    = "Australia Southeast"
 }
-module "cosmosdb-database-pocketpantrydb" {
-  source = "../modules/cosmos-db-database"
-  cosmosdb_database_name  = "pocketpantrydb"
-  cosmosdb_account_name   = "${module.cosmosdb.cosmosdb_account_name}"
-  resource_group_name     = "${module.resource-group.resource-group_name}"
-}
-module "cosmosdb-collection-allcollections" {
-  source = "../modules/cosmos-db-collection"
-  cosmosdb_collection_name   = "allcollections"
-  cosmosdb_database_name     = "${module.cosmosdb-database-pocketpantrydb.cosmosdb_database_name}"
-}
