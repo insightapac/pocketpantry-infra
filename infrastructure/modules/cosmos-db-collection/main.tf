d1a116480cc97f16c3bddb33284358ca51694fef
James Bannan <james@bannan.com.au>
2019-04-06 21:38:43 +1100

    Corrected command syntax

--- infrastructure/modules/cosmos-db-collection/main.tf
+++ infrastructure/modules/cosmos-db-collection/main.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "cosmosdb-collection" {
   provisioner "local-exec" {
-    command = "az cosmosdb collection create --db-name $(echo $cosmosdb_database_name) --collection-name $(echo $cosmosdb_collection_name) --name $(echo $cosmosdb_account_name) --resource-group-name $(echo $resource_group_name)--verbose"
+    command = "az cosmosdb collection create --db-name $(echo $cosmosdb_database_name) --collection-name $(echo $cosmosdb_collection_name) --name $(echo $cosmosdb_account_name) --resource-group-name $(echo $resource_group_name) --verbose"
     interpreter = ["/bin/bash", "-c"]
     environment {
       cosmosdb_account_name     = "${var.cosmosdb_account_name}"
